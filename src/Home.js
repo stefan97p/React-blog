b389bcee524b7b4d517dc5c152ec9d37fb31deff
Stefan Jokanovic <stefan97pro@gmail.com>
2021-10-12 01:16:24 +0200

    Json data base

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,29 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
+    const [blogs, setBlogs] = useState(null);
+
+    const handleDelete = (id) =>{
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+
+    }
+    useEffect(()=>{
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+            return res.json();
+        })
+        .then((data =>{
+            console.log(data);
+            setBlogs(data);
+
+        }))
+    },[]);
+    
     return ( 
-        <div className="home">
-            <h2>Homepage</h2>
+        <div className="home">          
+           {blogs && <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>}
         </div>
      );
 }
